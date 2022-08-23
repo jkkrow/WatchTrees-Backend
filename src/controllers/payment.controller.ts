4e5bfe27b69e7856092caa70ddd4aa9d537ff3fd
JK <dlord4242@gmail.com>
2022-08-23 19:35:35 +0900

    Get started with braintree

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,4 +1,13 @@
+import braintree from 'braintree';
+
 import { asyncHandler } from '../util/async-handler';
+
+const gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: '',
+  publicKey: '',
+  privateKey: '',
+});
 
 export const webhookEventHandler = asyncHandler(async (req, res) => {
   const webhookEvent = req.body;
